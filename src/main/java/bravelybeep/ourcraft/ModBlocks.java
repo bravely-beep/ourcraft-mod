18c04cc7017acc2ed579a67ccb4307a008b454cb
Joshua Walton <joshuawaltonm@gmail.com>
2024-12-15 17:10:21 +0000

    Added soft bedrock item

--- src/main/java/bravelybeep/ourcraft/ModBlocks.java
+++ src/main/java/bravelybeep/ourcraft/ModBlocks.java
@@ -3,6 +3,8 @@ package bravelybeep.ourcraft;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -13,6 +15,8 @@ public class ModBlocks {
 
 	public static Block register(Block block, String name) {
 		Identifier id = Identifier.of(Ourcraft.MOD_ID, name);
+		BlockItem blockItem = new BlockItem(block, new Item.Settings());
+		Registry.register(Registries.ITEM, id, blockItem);
 		return Registry.register(Registries.BLOCK, id, block);
 	}
 
